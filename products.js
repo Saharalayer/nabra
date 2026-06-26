6ce0226a547bec248b64f85bd978bfa4f3e4bb19
Saharalayer <saharalayer@outlook.com>
2026-06-26 10:17:30 +0100

    Create products.js

---
+++ products.js
@@ -0,0 +1,23 @@
+const products = [
+
+{
+id:1,
+name:"Eye Pearl Elixir",
+price:199,
+oldPrice:249,
+image:"images/product1.jpg",
+category:"Eye Care",
+description:"وصف المنتج"
+},
+
+{
+id:2,
+name:"Vitamin C Serum",
+price:149,
+oldPrice:199,
+image:"images/product2.jpg",
+category:"Skin Care",
+description:"وصف المنتج"
+}
+
+];
